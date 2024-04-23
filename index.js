b1fb718ab42a94484294e16e5ff66fc15561f081
AyoRichie1-ops <onanusiabdullateef@gmail.com>
2024-04-23 12:08:11 +0100

    added error route in index.js

--- index.js
+++ index.js
@@ -5,9 +5,23 @@ const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const taskRouter = require("./routes/taskRoutes");
 
+// middlewares
 app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to Goals Api!" });
+});
+
 app.use("/api/v1/tasks", taskRouter);
 
+// Error Routes
+
+app.use((req, res) => {
+  res.status(404).json({ meessage: "Resource not found" });
+});
+
+// db Connections and server listening
 const start = async () => {
   try {
     await mongoose.connect(process.env.dbURI, {
